f493c9f61893dce36ae226a756963e48ece63b4a
CorgiKing <1030185489@qq.com>
2018-07-27 10:59:33 +0800

    添加gitignore

--- djdemo/urls.py
+++ djdemo/urls.py
@@ -1,4 +1,4 @@
-"""garden URL Configuration
+"""djdemo URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.11/topics/http/urls/
